1d20dcfb5f0b0d98e60c5f915818be959adc9c80
Erik Madsen <ekmadsen@comcast.net>
2019-01-04 16:18:13 -0600

    Adding source code.  Previous commit only added .gitignore file.

---
+++ Service Proxy/ProxyMessageHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using ErikTheCoder.ServiceContract;
+
+
+namespace ErikTheCoder.ServiceProxy
+{
+    public class ProxyMessageHandler : DelegatingHandler
+    {
+        private readonly Func<Guid> _getCorrelationId;
+
+
+        public ProxyMessageHandler(Func<Guid> GetCorrelationId)
+        {
+            _getCorrelationId = GetCorrelationId;
+            InnerHandler = new HttpClientHandler();
+        }
+
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage Request, CancellationToken CancellationToken)
+        {
+            if (!Request.Headers.Contains(CustomHttpHeader.CorrelationId))
+            {
+                Request.Headers.Add(CustomHttpHeader.CorrelationId, _getCorrelationId().ToString());
+            }
+            return await base.SendAsync(Request, CancellationToken);
+        }
+    }
+}
---
+++ Service Proxy/ServiceProxies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using JetBrains.Annotations;
+using Refit;
+
+
+namespace ErikTheCoder.ServiceProxy
+{
+    [UsedImplicitly]
+    public static class Proxy
+    {
+        [UsedImplicitly]
+        public static T For<T>(string ServiceUrlRoot, string Jwt, Func<Guid> GetCorrelationId)
+        {
+            ProxyMessageHandler proxyMessageHandler = new ProxyMessageHandler(GetCorrelationId);
+            HttpClient httpClient = new HttpClient(proxyMessageHandler) { BaseAddress = new Uri(ServiceUrlRoot) };
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {Jwt}");
+            return RestService.For<T>(httpClient);
+        }
+    }
+}
